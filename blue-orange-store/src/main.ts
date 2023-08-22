2ba8c8ce2103253b5ba0c303490f1c0520411489
Adrian Guevara <adrianjga96@gmail.com>
2023-08-22 06:30:56 -0400

    adding global interceptor by use @Exclude() decorator

--- blue-orange-store/src/main.ts
+++ blue-orange-store/src/main.ts
@@ -1,5 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -14,6 +14,7 @@ async function bootstrap() {
       },
     }),
   );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   const config = new DocumentBuilder()
     .setTitle('Blue&Orange Store')
     .setDescription('Blue&Orange Store API')
