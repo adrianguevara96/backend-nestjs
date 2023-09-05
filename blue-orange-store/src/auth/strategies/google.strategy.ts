d0db3426858cdbaebdca2110e5d286bb071c8466
Adrian Guevara <adrianjga96@gmail.com>
2023-09-05 12:34:18 -0400

    eslint

--- blue-orange-store/src/auth/strategies/google.strategy.ts
+++ blue-orange-store/src/auth/strategies/google.strategy.ts
@@ -1,7 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
-
 import config from './../../config';
 import { ConfigType } from '@nestjs/config';
 
